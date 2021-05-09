fe2b79b6bad3962163207d98b07caa965982a27d
xonix <xonixx@gmail.com>
2021-05-09 18:27:27 +0300

    Start with "Hello world" bot with basic code setup #1 - init from spring initializr

---
+++ src/main/kotlin/com/cmlteam/bookmark_telegram_bot/BookmarkTelegramBotApplication.kt
@@ -0,0 +1,11 @@
+package com.cmlteam.bookmark_telegram_bot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookmarkTelegramBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookmarkTelegramBotApplication>(*args)
+}
