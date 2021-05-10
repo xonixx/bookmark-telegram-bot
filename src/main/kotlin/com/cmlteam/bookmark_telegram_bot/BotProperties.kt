01ab76f961dfd82d1e7b662279d39bb3e3fb7b6b
xonix <xonixx@gmail.com>
2021-05-11 02:53:07 +0300

    Start with "Hello world" bot with basic code setup #1

--- src/main/kotlin/com/cmlteam/bookmark_telegram_bot/BotProperties.kt
+++ src/main/kotlin/com/cmlteam/bookmark_telegram_bot/BotProperties.kt
@@ -3,22 +3,20 @@ package com.cmlteam.bookmark_telegram_bot
 import com.pengrad.telegrambot.model.User
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.context.annotation.Configuration
 import org.springframework.validation.annotation.Validated
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 
 
-@Configuration
 @ConfigurationProperties(prefix = "telegram-bot")
-//@ConstructorBinding
+@ConstructorBinding
 @Validated
 data class BotProperties(
-    @field:Positive override var adminUser: Long = 0,
-    @field:NotBlank var token: String? = null,
-    @field:NotBlank var backupFolder: String? = null,
-    @field:NotNull var maxFileSize: Int? = null
+    @field:Positive override val adminUser: Long = 0,
+    @field:NotBlank val token: String? = null,
+    @field:NotBlank val backupFolder: String? = null,
+    @field:NotNull val maxFileSize: Int? = null
 ) : AdminUserChecker {
 
     override fun isAdmin(userId: Long): Boolean {
